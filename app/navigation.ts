c39d995a49f0328a07154e3a2be26cdf2081dc39
steven <hhuang@ban-qu.com>
2024-07-07 01:50:34 -0400

    initial commit

--- app/navigation.ts
+++ app/navigation.ts
@@ -1,8 +1,6 @@
 import { createSharedPathnamesNavigation } from 'next-intl/navigation';
-
 import { locales } from '../i18n';
 
 export const localePrefix = 'as-needed';
 
-// eslint-disable-next-line object-curly-newline
 export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation({ locales, localePrefix });
