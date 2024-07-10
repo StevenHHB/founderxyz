5fd1a5c613bb60a22ddbb0ffb72477b22c86d915
steven <hhuang@ban-qu.com>
2024-07-09 23:04:36 -0400

    update category refresh bug

--- app/[locale]/(with-footer)/category/[code]/page/[pageNum]/page.tsx
+++ app/[locale]/(with-footer)/category/[code]/page/[pageNum]/page.tsx
@@ -26,6 +26,8 @@ export async function generateMetadata({ params }: { params: { code: string; pag
 export default async function Page({ params }: { params: { code: string; pageNum?: string } }) {
   const supabase = createClient();
   const currentPage = Number(params?.pageNum || 1);
+  const startRange = (currentPage - 1) * InfoPageSize;
+  const endRange = currentPage * InfoPageSize - 1;
 
   const [{ data: categoryList }, { data: navigationList, count }] = await Promise.all([
     supabase.from('navigation_category').select().eq('name', params.code),
@@ -33,7 +35,7 @@ export default async function Page({ params }: { params: { code: string; pageNum
       .from('web_navigation')
       .select('*', { count: 'exact' })
       .eq('category_name', params.code)
-      .range(0, InfoPageSize - 1),
+      .range(startRange, endRange),
   ]);
 
   if (!categoryList || !categoryList[0]) {
