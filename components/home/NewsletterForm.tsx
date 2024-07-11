ce7f820bca17b7bed36774a3cc22ccb74a122f01
steven <hhuang@ban-qu.com>
2024-07-11 02:34:03 -0400

    add newletterform try

--- components/home/NewsletterForm.tsx
+++ components/home/NewsletterForm.tsx
@@ -13,106 +13,107 @@ import { Input } from '@/components/ui/input';
 import Spinning from '@/components/Spinning';
 
 const FormSchema = z.object({
-    name: z.string().min(1, 'Name is required'),
-    email: z.string().email('Invalid email address'),
+  name: z.string().min(1, 'Name is required'),
+  email: z.string().email('Invalid email address'),
 });
 
 export default function NewsletterForm({ className }: { className?: string }) {
-    const supabase = createClient();
-    const t = useTranslations('Newsletter');
+  const supabase = createClient();
+  const t = useTranslations('Newsletter');
 
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-            name: '',
-            email: '',
-        },
-    });
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+    },
+  });
 
-    const onSubmit = async (formData: z.infer<typeof FormSchema>) => {
-        let errMsg: any = t('networkError');
-        try {
-            setLoading(true);
-            const { error } = await supabase.from('newsletter_subscriptions').insert({
-                name: formData.name,
-                email: formData.email,
-            });
-            if (error) {
-                errMsg = error.message;
-                throw new Error();
-            }
-            toast.success(t('success'));
-            form.reset();
-        } catch (error) {
-            toast.error(errMsg);
-        } finally {
-            setLoading(false);
-        }
-    };
+  const onSubmit = async (formData: z.infer<typeof FormSchema>) => {
+    let errMsg: any = t('networkError');
+    try {
+      setLoading(true);
+      const { error } = await supabase.from('newsletter_subscriptions').insert({
+        name: formData.name,
+        email: formData.email,
+      });
+      if (error) {
+        errMsg = error.message;
+        throw new Error();
+      }
+      toast.success(t('success'));
+      form.reset();
+    } catch (error) {
+      toast.error(errMsg);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    return (
-        <Form {...form}>
-            <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className={`mx-3 mb-5 flex flex-col justify-between rounded-[12px] bg-[#2C2D36] px-3 py-5 lg:w-[444px] lg:p-8 ${className}`}
-            >
-                <div className='space-y-3 lg:space-y-5'>
-                    <FormField
-                        control={form.control}
-                        name='name'
-                        render={({ field }) => (
-                            <FormItem className='space-y-1'>
-                                <FormLabel>{t('name')}</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        placeholder='Your Name'
-                                        className='input-border-pink h-[42px] w-full rounded-[8px] border-[0.5px] bg-dark-bg p-5'
-                                        value={field.value}
-                                        onChange={field.onChange}
-                                        onBlur={field.onBlur}
-                                        name={field.name}
-                                        ref={field.ref}
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name='email'
-                        render={({ field }) => (
-                            <FormItem className='space-y-1'>
-                                <FormLabel>{t('email')}</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        placeholder='Your Email'
-                                        className='input-border-pink h-[42px] w-full rounded-[8px] border-[0.5px] bg-dark-bg p-5'
-                                        value={field.value}
-                                        onChange={field.onChange}
-                                        onBlur={field.onBlur}
-                                        name={field.name}
-                                        ref={field.ref}
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                </div>
-                <div className='flex flex-col gap-[10px] lg:gap-8'>
-                    <button
-                        type='submit'
-                        disabled={loading}
-                        className={`flex-center mt-auto h-[48px] w-full gap-4 rounded-[8px] bg-white text-center font-bold text-black hover:cursor-pointer hover:opacity-80 ${loading && 'hover:cursor-not-allowed'
-                            }`}
-                    >
-                        {loading ? <Spinning className='size-[22px]' /> : t('subscribe')}
-                    </button>
-                </div>
-            </form>
-        </Form>
-    );
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={`mx-3 mb-5 flex flex-col justify-between rounded-[12px] bg-[#2C2D36] px-3 py-5 lg:w-[444px] lg:p-8 ${className}`}
+      >
+        <div className='space-y-3 lg:space-y-5'>
+          <FormField
+            control={form.control}
+            name='name'
+            render={({ field }) => (
+              <FormItem className='space-y-1'>
+                <FormLabel>{t('name')}</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='Your Name'
+                    className='input-border-pink h-[42px] w-full rounded-[8px] border-[0.5px] bg-dark-bg p-5'
+                    value={field.value}
+                    onChange={field.onChange}
+                    onBlur={field.onBlur}
+                    name={field.name}
+                    ref={field.ref}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem className='space-y-1'>
+                <FormLabel>{t('email')}</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='Your Email'
+                    className='input-border-pink h-[42px] w-full rounded-[8px] border-[0.5px] bg-dark-bg p-5'
+                    value={field.value}
+                    onChange={field.onChange}
+                    onBlur={field.onBlur}
+                    name={field.name}
+                    ref={field.ref}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className='flex flex-col gap-[10px] lg:gap-8'>
+          <button
+            type='submit'
+            disabled={loading}
+            className={`flex-center mt-auto h-[48px] w-full gap-4 rounded-[8px] bg-white text-center font-bold text-black hover:cursor-pointer hover:opacity-80 ${
+              loading && 'hover:cursor-not-allowed'
+            }`}
+          >
+            {loading ? <Spinning className='size-[22px]' /> : t('subscribe')}
+          </button>
+        </div>
+      </form>
+    </Form>
+  );
 }
