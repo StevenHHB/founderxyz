4a416925c47306d070a36a58fbb43d3a0973634f
steven <hhuang@ban-qu.com>
2024-07-11 14:39:33 -0400

    add newletterform try

--- components/home/NewsletterForm.tsx
+++ components/home/NewsletterForm.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { createClient } from '@/db/supabase/client';
-import { useForm, SubmitHandler } from 'react-hook-form';
+import { useForm, SubmitHandler, FieldValues } from 'react-hook-form';
 import { toast } from 'sonner';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -55,7 +55,7 @@ export default function NewsletterForm({ className }: { className?: string }) {
   };
 
   return (
-    <Form control={form.control}>
+    <Form>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
         className={`mx-3 mb-5 flex flex-col justify-between rounded-[12px] bg-[#2C2D36] px-3 py-5 lg:w-[444px] lg:p-8 ${className}`}
